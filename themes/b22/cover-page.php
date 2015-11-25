6f2ef75059656ac4e9242756ec9746b0b0e3f0bc
aarongillett <mail@aarongillett.com>
2015-11-25 15:56:21 +0100

    Index feature image

--- themes/b22/cover-page.php
+++ themes/b22/cover-page.php
@@ -10,8 +10,11 @@ Template Name: cover page template
 
 <?php
 // check if the post or page has a Featured Image assigned to it.
-if ( has_post_thumbnail() ) {
-    the_post_thumbnail();
+if (has_post_thumbnail( $post->ID ) ){
+	$feat_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+	$feat_img_src = $feat_img[0];
+}else{
+	$feat_img_src = "http://placehold.it/1024x768/";
 }
 ?>
 
