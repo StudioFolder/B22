6aaa06d9c691bffad1f918add05f7160536cf3f7
aarongillett <mail@aarongillett.com>
2015-11-30 17:02:19 +0100

    Profile

--- themes/b22/profile-page.php
+++ themes/b22/profile-page.php
@@ -15,23 +15,14 @@ Template Name: profile page template
 <div id="page_wrap" class="container home">
 
 	<div class="row">
-	        	<?php the_content(); ?>
+
 		<div class="col-xs-12"> <!-- Full-width column -->
-
-	        <div class="entry">
 	        	<?php the_content(); ?>
-	    	</div>
-
 		</div>
 
 		<div class="col-sm-6"> <!-- Half-width column -->
-
-			<div class="col-sm-6 profile_details"> <!-- Half-width column with class profile_details -->
-
 				<?php the_field('profile_details'); ?> <!-- get profile_details text -->
 				<?php?>
-
-			</div>
 		</div>
 
 	</div>
