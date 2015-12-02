9c38a0aabb4f57ad489127d5913ab35ebc034ff5
aarongillett <mail@aarongillett.com>
2015-12-02 17:24:28 +0100

    cover

--- themes/b22/cover-page.php
+++ themes/b22/cover-page.php
@@ -6,19 +6,6 @@ Template Name: cover page template
 
 <?php get_header(); ?>
 
-
-<?php
-// check if the post or page has a Featured Image assigned to it.
-if (has_post_thumbnail( $post->ID ) ){
-	$feat_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
-	$feat_img_src = $feat_img[0];
-}else{
-	$feat_img_src = "http://placehold.it/1024x768/";
-}
-?>
-
-
-
 <?php if (have_posts()) : ?>
 
 <?php while (have_posts()) : the_post(); ?>
